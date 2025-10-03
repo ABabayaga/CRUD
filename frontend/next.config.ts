88100edfb68339d83e7ff9f737cd120c695723e4
ABabayaga <alef_rdl@hotmail.com>
2025-10-03 16:00:16 -0400

    ajuste next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    // Não bloqueia o build se houver erros de lint
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
