0a9f4e97df1e970ecd0784e464fc656b67fcc28c
ABabayaga <alef_rdl@hotmail.com>
2025-10-03 16:32:25 -0400

    ajuste main.ts cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -15,7 +15,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: [process.env.CORS_ORIGIN || 'http://localhost:3000'],
+    origin: ['http://localhost:3000', 'https://crud-ten-ebon.vercel.app'],
     credentials: true,
   });
 
@@ -51,6 +51,6 @@ async function bootstrap() {
     );
   }
 
-  await app.listen(Number(process.env.PORT) || 3002);
+  await app.listen(process.env.PORT ?? 3002, '0.0.0.0');
 }
 bootstrap();
